a9af4fea1f74ea41bafab40eb80b862c421b65ff
coynemt2 <theodore.coynemorgan2@mail.dcu.ie>
2021-01-18 10:15:20 +0000

    Add preliminary command line commands using Cobra Add basic command go files to client and initialize Cobra

---
+++ src/synche/client/cli/cmd/upload.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+var uploadCmd = &cobra.Command{
+	Use:   "upload",
+	Short: "<Short description here>",
+	Long:  `<Long description here>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		uploadJob()
+	},
+}
+
+func uploadJob() {
+	// retrieve a list of files that are on the server and return them here
+}
--- src/synche/client/go.mod
+++ src/synche/client/go.mod
@@ -2,4 +2,8 @@ module client
 
 go 1.15
 
-require github.com/kalafut/imohash v1.0.0
+require (
+	github.com/kalafut/imohash v1.0.0
+	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.0
+)
